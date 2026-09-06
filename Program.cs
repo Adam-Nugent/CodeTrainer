2e5a48eb1ac6c80152f37b9a486cd5ed6d546e47
Adam Nugent <37636065+Adam-Nugent@users.noreply.github.com>
2026-09-06 20:01:20 +0100

    Add score tracking to typing exercise

--- Program.cs
+++ Program.cs
@@ -3,6 +3,8 @@ Console.WriteLine("===========");
 Console.WriteLine();
 
 string targetCode = "Console.WriteLine(\"Hello, World!\");";
+int score = 0;
+int totalQuestions = 1;
 
 Console.WriteLine("Type the statement exactly as shown:");
 Console.WriteLine(targetCode);
@@ -15,9 +17,12 @@ bool isCorrect = userInput == targetCode;
 
 if (isCorrect)
 {
+    score = score + 1;
     Console.WriteLine("Correct!");
 }
 else
 {
     Console.WriteLine("Incorrect.");
 }
+
+Console.WriteLine($"Score: {score}/{totalQuestions}");
